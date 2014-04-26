552a67acdb9e145ae9a1673b5c89387bb153facc
Idan Haviv <idan.haviv@gmail.com>
2014-04-26 13:55:52 +0300

    start & stop buttons functionality works end to end.

--- clientTaxiCab/src/com/idan/clienttaxicab/ShowLocationActivity.java
+++ clientTaxiCab/src/com/idan/clienttaxicab/ShowLocationActivity.java
@@ -1,54 +1,104 @@
 package com.idan.clienttaxicab;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class ShowLocationActivity extends Activity  {
-	  
-	/** Called when the activity is first created. */
+public class ShowLocationActivity extends Activity  
+{
 	private Button btnStartService;
 	private Button btnStopService;
 	private Intent intent;
 
 	  @Override
-	  public void onCreate(Bundle savedInstanceState) {
+	  public void onCreate(Bundle savedInstanceState) 
+	  {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_show_location);
-//	    latituteField = (TextView) findViewById(R.id.TextView02);
-//	    longitudeField = (TextView) findViewById(R.id.TextView04);
 	    
 	    btnStartService = (Button) findViewById(R.id.buttonStart);
-	    btnStartService.setOnClickListener(new View.OnClickListener() {
-			
+	    btnStartService.setOnClickListener(new View.OnClickListener() 
+	    {			
 			@Override
-			public void onClick(View arg0) {	
-				
+			public void onClick(View arg0) 
+			{	
 				intent = new Intent(getBaseContext(), UpdateServerOnLocationChangeService.class);
 			    startService(intent);
 		        Toast.makeText(getApplicationContext(), "starting service", Toast.LENGTH_SHORT).show();
-
-				
 			}
 		});
 	    
 	    btnStopService = (Button) findViewById(R.id.buttonStop);
-	    btnStopService.setOnClickListener(new View.OnClickListener() {
-			
+	    btnStopService.setOnClickListener(new View.OnClickListener() 
+	    {			
 			@Override
-			public void onClick(View arg0) {	
-			
+			public void onClick(View arg0) 
+			{
+				SendStopSignalToServerTask sendStopSignalToServerTask = new SendStopSignalToServerTask();
+				String androidID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+				sendStopSignalToServerTask.execute(androidID);
 			    stopService(new Intent(getBaseContext(), UpdateServerOnLocationChangeService.class));
 		        Toast.makeText(getApplicationContext(), "stopping service", Toast.LENGTH_SHORT).show();
-
-				
 			}
 		});
-	  
 	  }
+	  
+	  private class SendStopSignalToServerTask extends AsyncTask<String, Void, Void>// TODO do do
+	  {
+			@Override
+			protected Void doInBackground(String... deviceIDs){			
+				try {				
+					for(String deviceID : deviceIDs){
+						sendStopSignalToServer(deviceID);
+					}
+				} catch (Exception e){
+					e.printStackTrace();
+				}
+				return null;
+			}
+	  }
+	  
+	  public void sendStopSignalToServer(String deviceID) 
+	  {
+			final HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(
+					"http://sharetaxi6.appspot.com/stopDevice");
+
+			try {
+				// Add your data
+				List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+				pairs.add(new BasicNameValuePair("androidID", deviceID));
+				httppost.setEntity(new UrlEncodedFormEntity(pairs));
+
+				// Execute HTTP Post Request
+				HttpResponse response = httpclient.execute(httppost); //TODO handle response from server
+
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+
+    	}
+	  
 	  
 	  
       
--- clientTaxiCab/src/com/idan/clienttaxicab/UpdateServerOnLocationChangeService.java
+++ clientTaxiCab/src/com/idan/clienttaxicab/UpdateServerOnLocationChangeService.java
@@ -1,44 +1,23 @@
 package com.idan.clienttaxicab;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.widget.Toast;
 
 public class UpdateServerOnLocationChangeService extends Service{
 	
-	private Thread serviceThread;
+//	private Thread serviceThread;
 	
 	private final long MINIMUM_TIME_FOR_UPDATE = 1000;//by milliseconds
 	private final long MINIMUM_DISTANCE_FOR_UPDATE = 1;//by meters
 	
-    private ServiceLocationListener locationListener;
+    private GPSLocationListener locationListener;
     private LocationManager locationManager;
-    private String provider;
-    private Intent intent;
+//    private String provider;
+//    private Intent intent;
     private boolean isActive = false;
     
 	
@@ -49,9 +28,9 @@ public class UpdateServerOnLocationChangeService extends Service{
 	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 	    // Define the criteria how to select the location provider -> use
 	    // default
-	    Criteria criteria = new Criteria();
-	    provider = locationManager.getBestProvider(criteria, false);
-		locationListener = new ServiceLocationListener(getBaseContext());
+//	    Criteria criteria = new Criteria();
+//	    provider = locationManager.getBestProvider(criteria, false);
+		locationListener = new GPSLocationListener(getBaseContext());
 		
 //		serviceThread = new Thread(new Runnable() {
 //	        public void run() {
@@ -94,94 +73,9 @@ public class UpdateServerOnLocationChangeService extends Service{
 	}
 
 	
-	public class ServiceLocationListener implements LocationListener{
-		
-		private Context context;
-		private String AndroidId;
-		
-		public ServiceLocationListener(Context context){
-			this.context = context;
-		}
-		
-		@Override
-		  public void onLocationChanged(Location location) {
-		    double lat = location.getLatitude();
-		    double lng = location.getLongitude();
-		    AndroidId = Settings.Secure.getString(getContentResolver(),
-       	         Settings.Secure.ANDROID_ID);
-		    //sendLocToServer(new DataStructure(lat, lng, AndroidId, "4", "N"));
-		    UpdateServerTask serverTask = new UpdateServerTask();
-		    DataStructure dataStructure = new DataStructure(lat, lng, AndroidId, "4", "N");
-		    serverTask.execute(new DataStructure[]{dataStructure});
-		  }
-
-		  @Override
-		  public void onStatusChanged(String provider, int status, Bundle extras) {
-		    // TODO Auto-generated method stub
-
-		  }
-
-		  @Override
-		  public void onProviderEnabled(String provider) {
-		    Toast.makeText(this.context, "Enabled new provider " + provider,
-		        Toast.LENGTH_SHORT).show();
-
-		  }
-
-		  @Override
-		  public void onProviderDisabled(String provider) {
-		    Toast.makeText(this.context, "Disabled provider " + provider,
-		        Toast.LENGTH_SHORT).show();
-		  }
-	}
 	
-	public void sendLocToServer(DataStructure curr_location) {
-
-		final HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httppost = new HttpPost(
-				"http://sharetaxi6.appspot.com/postLocation");
-
-		try {
-			// Add your data
-			String longitudeStr = String.valueOf(curr_location.longitude);
-			String latitudeStr = String.valueOf(curr_location.latitude);
-
-			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-			pairs.add(new BasicNameValuePair("longitude", longitudeStr));
-			pairs.add(new BasicNameValuePair("latitude", latitudeStr));
-			pairs.add(new BasicNameValuePair("androidID", curr_location.AndroidID));
-			pairs.add(new BasicNameValuePair("lineNum", curr_location.lineNum));
-			pairs.add(new BasicNameValuePair("direction", curr_location.direction));
-			httppost.setEntity(new UrlEncodedFormEntity(pairs));
-			
-			//Toast.makeText(getBaseContext(), "gps info gathered by now is: long "+ longitudeStr + ", lat " + latitudeStr, Toast.LENGTH_SHORT).show();
-			
-			// Execute HTTP Post Request
-			HttpResponse response = httpclient.execute(httppost);
-
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-		}
-
-	}
-	  
-	private class UpdateServerTask extends AsyncTask<DataStructure, Void, Void>{
-
-		@Override
-		protected Void doInBackground(DataStructure... dataStructures){			
-			try {				
-				for(DataStructure dataStructure : dataStructures){
-					sendLocToServer(dataStructure);
-				}
-			} catch (Exception e){
-				e.printStackTrace();
-			}
-			
-			return null;
-		}
-		
-	}
+	
+		  
+	
 	
 }
