c9d3a16f96a61402717631c0f37e6a313b4c4f1b
Idan Haviv <idan.haviv@gmail.com>
2014-04-26 22:39:05 +0300

    added a menu for line number selection, adn a text box that presents the current state (on/off) sending line number to server

--- clientTaxiCab/src/com/idan/clienttaxicab/ShowLocationActivity.java
+++ clientTaxiCab/src/com/idan/clienttaxicab/ShowLocationActivity.java
@@ -18,104 +18,188 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class ShowLocationActivity extends Activity  
-{
+public class ShowLocationActivity extends Activity {
+	
 	private Button btnStartService;
 	private Button btnStopService;
 	private Intent intent;
+	protected String lineNumber = null;
+	protected String currentState;
+	private boolean enableMenu = true;
+	private TextView currentStateTextView;
 
-	  @Override
-	  public void onCreate(Bundle savedInstanceState) 
-	  {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_show_location);
-	    
-	    btnStartService = (Button) findViewById(R.id.buttonStart);
-	    btnStartService.setOnClickListener(new View.OnClickListener() 
-	    {			
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_location);
+		this.currentState = "Offline";
+
+		btnStartService = (Button) findViewById(R.id.buttonStart);
+		btnStartService.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View arg0) 
-			{	
-				intent = new Intent(getBaseContext(), UpdateServerOnLocationChangeService.class);
-			    startService(intent);
-		        Toast.makeText(getApplicationContext(), "starting service", Toast.LENGTH_SHORT).show();
-			}
-		});
-	    
-	    btnStopService = (Button) findViewById(R.id.buttonStop);
-	    btnStopService.setOnClickListener(new View.OnClickListener() 
-	    {			
-			@Override
-			public void onClick(View arg0) 
-			{
-				SendStopSignalToServerTask sendStopSignalToServerTask = new SendStopSignalToServerTask();
-				String androidID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-				sendStopSignalToServerTask.execute(androidID);
-			    stopService(new Intent(getBaseContext(), UpdateServerOnLocationChangeService.class));
-		        Toast.makeText(getApplicationContext(), "stopping service", Toast.LENGTH_SHORT).show();
-			}
-		});
-	  }
-	  
-	  private class SendStopSignalToServerTask extends AsyncTask<String, Void, Void>// TODO do do
-	  {
-			@Override
-			protected Void doInBackground(String... deviceIDs){			
-				try {				
-					for(String deviceID : deviceIDs){
-						sendStopSignalToServer(deviceID);
-					}
-				} catch (Exception e){
-					e.printStackTrace();
+			public void onClick(View arg0) {
+				if(verifyLineNumberSelected()){
+					setOnlineState();
+					intent = new Intent(getBaseContext(),
+							UpdateServerOnLocationChangeService.class);
+					intent.putExtra("lineNumber", lineNumber);
+					startService(intent);
+					Toast.makeText(getApplicationContext(), "starting service",
+							Toast.LENGTH_SHORT).show();// TODO add some static
+														// notice for the current
+														// app situation
 				}
-				return null;
 			}
-	  }
-	  
-	  public void sendStopSignalToServer(String deviceID) 
-	  {
-			final HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost(
-					"http://sharetaxi6.appspot.com/stopDevice");
+		});
 
+		btnStopService = (Button) findViewById(R.id.buttonStop);
+		btnStopService.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				lineNumber = null;
+				setOffineState();
+				SendStopSignalToServerTask sendStopSignalToServerTask = new SendStopSignalToServerTask();
+				String androidID = Settings.Secure.getString(
+						getContentResolver(), Settings.Secure.ANDROID_ID);
+				sendStopSignalToServerTask.execute(androidID);
+				stopService(new Intent(getBaseContext(),
+						UpdateServerOnLocationChangeService.class));
+				Toast.makeText(getApplicationContext(), "stopping service",
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		currentStateTextView = (TextView) findViewById(R.id.currentState);
+		currentStateTextView.setText(currentState);
+	}
+
+	protected void setOffineState() {
+		enableLineNumberSelection();
+		currentState = "Offline";
+		currentStateTextView.setText(currentState);
+	}
+
+	protected boolean verifyLineNumberSelected() {
+		if(lineNumber == null){
+			Toast.makeText(getApplicationContext(), "Please select a line number",
+					Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		
+		return true;
+	}
+
+	protected void setOnlineState() {
+		disableLineNumberSelection();
+		currentState = "Online";
+		currentStateTextView.setText(currentState);
+	}
+
+	protected void enableLineNumberSelection() {
+		this.enableMenu = true;
+	}
+
+	protected void disableLineNumberSelection() {
+		this.enableMenu = false;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.setGroupEnabled(R.id.groupMenuItems, enableMenu);
+		return true;
+	}
+
+	private class SendStopSignalToServerTask extends
+			AsyncTask<String, Void, Void>// TODO do do
+	{
+		@Override
+		protected Void doInBackground(String... deviceIDs) {
 			try {
-				// Add your data
-				List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-				pairs.add(new BasicNameValuePair("androidID", deviceID));
-				httppost.setEntity(new UrlEncodedFormEntity(pairs));
-
-				// Execute HTTP Post Request
-				HttpResponse response = httpclient.execute(httppost); //TODO handle response from server
-
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				for (String deviceID : deviceIDs) {
+					sendStopSignalToServer(deviceID);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+			return null;
+		}
+	}
 
-    	}
-	  
-	  
-	  
-      
-      
-      
+	public void sendStopSignalToServer(String deviceID) {
+		final HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(
+				"http://sharetaxi6.appspot.com/stopDevice");
 
-	  /* Request updates at startup */
-//	  @Override
-//	  protected void onResume() {
-//	    super.onResume();
-//	    Intent locationServiceIntent = new Intent(this, UpdateServerOnLocationChangeService.class);
-//	    this.startService(locationServiceIntent);
-//	  }
-//
-//	  /* Remove the locationlistener updates when Activity is paused */
-//	  @Override
-//	  protected void onPause() {
-//	    super.onPause();
-//	  }
-} 
+		try {
+			// Add your data
+			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+			pairs.add(new BasicNameValuePair("androidID", deviceID));
+			httppost.setEntity(new UrlEncodedFormEntity(pairs));
+
+			// Execute HTTP Post Request
+			HttpResponse response = httpclient.execute(httppost); // TODO handle
+																	// response
+																	// from
+																	// server
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.show_location, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.four:
+			Toast.makeText(this, "You selected line number 4",
+					Toast.LENGTH_SHORT).show();
+			this.lineNumber = "4";
+			break;
+		case R.id.fourA:
+			Toast.makeText(this, "You selected line number 4a",
+					Toast.LENGTH_SHORT).show();
+			this.lineNumber = "4a";
+			break;
+		case R.id.five:
+			Toast.makeText(this, "You selected line number 5",
+					Toast.LENGTH_SHORT).show();
+			this.lineNumber = "5";
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/* Request updates at startup */
+	// @Override
+	// protected void onResume() {
+	// super.onResume();
+	// Intent locationServiceIntent = new Intent(this,
+	// UpdateServerOnLocationChangeService.class);
+	// this.startService(locationServiceIntent);
+	// }
+	//
+	// /* Remove the locationlistener updates when Activity is paused */
+	// @Override
+	// protected void onPause() {
+	// super.onPause();
+	// }
+}
--- clientTaxiCab/src/com/idan/clienttaxicab/UpdateServerOnLocationChangeService.java
+++ clientTaxiCab/src/com/idan/clienttaxicab/UpdateServerOnLocationChangeService.java
@@ -7,75 +7,77 @@ import android.location.LocationManager;
 import android.os.IBinder;
 import android.widget.Toast;
 
-public class UpdateServerOnLocationChangeService extends Service{
-	
-//	private Thread serviceThread;
-	
-	private final long MINIMUM_TIME_FOR_UPDATE = 1000;//by milliseconds
-	private final long MINIMUM_DISTANCE_FOR_UPDATE = 1;//by meters
-	
-    private GPSLocationListener locationListener;
-    private LocationManager locationManager;
-//    private String provider;
-//    private Intent intent;
-    private boolean isActive = false;
-    
-	
+public class UpdateServerOnLocationChangeService extends Service {
+
+	// private Thread serviceThread;
+
+	private final long MINIMUM_TIME_FOR_UPDATE = 1000;// by milliseconds
+	private final long MINIMUM_DISTANCE_FOR_UPDATE = 1;// by meters
+
+	private GPSLocationListener locationListener;
+	private LocationManager locationManager;
+	// private String provider;
+	// private Intent intent;
+	private boolean isActive = false;
+	private String lineNumber;
+
 	@Override
-	public void onCreate() 
-	{
+	public void onCreate() {
 		// Get the location manager
-	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	    // Define the criteria how to select the location provider -> use
-	    // default
-//	    Criteria criteria = new Criteria();
-//	    provider = locationManager.getBestProvider(criteria, false);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// Define the criteria how to select the location provider -> use
+		// default
+		// Criteria criteria = new Criteria();
+		// provider = locationManager.getBestProvider(criteria, false);
+
 		locationListener = new GPSLocationListener(getBaseContext());
-		
-//		serviceThread = new Thread(new Runnable() {
-//	        public void run() {
-//	        	locationManager.requestLocationUpdates(provider, 1, 1, locationListener);
-//	        }
-//	    });
-		
-	 }
 
-	  @Override
-	  public int onStartCommand(Intent intent, int flags, int startId) 
-	  {
-		  if(isActive == false){
-			  isActive = true;
-			  Toast.makeText(getBaseContext(), "service ************************  starting", Toast.LENGTH_SHORT).show();
+		// serviceThread = new Thread(new Runnable() {
+		// public void run() {
+		// locationManager.requestLocationUpdates(provider, 1, 1,
+		// locationListener);
+		// }
+		// });
 
-//		      serviceThread.start();
-		      
-		      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MINIMUM_TIME_FOR_UPDATE, MINIMUM_DISTANCE_FOR_UPDATE, locationListener);
+	}
 
-		      // If we get killed, after returning from here, restart
-		      return START_STICKY;
-		  } else {
-			  return START_STICKY;
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if (isActive == false) {
+			isActive = true;
+			this.lineNumber = intent.getStringExtra("lineNumber");
+			locationListener.setLineNumber(this.lineNumber);
+			Toast.makeText(getBaseContext(),
+					"service ************************  starting",
+					Toast.LENGTH_SHORT).show();
+
+			// serviceThread.start();
+
+			locationManager.requestLocationUpdates(
+					LocationManager.GPS_PROVIDER, MINIMUM_TIME_FOR_UPDATE,
+					MINIMUM_DISTANCE_FOR_UPDATE, locationListener);
+
+			// If we get killed, after returning from here, restart
+			return START_STICKY;
+		} else {
+			return START_STICKY;
 		}
-	      
-	  }
 
-	  @Override
-	  public void onDestroy() {
-		  locationManager.removeUpdates(locationListener);
-		  super.onDestroy();
-	      //Toast.makeText(getApplicationContext(), "stopped sending location updates to ShareTaxiServer", Toast.LENGTH_SHORT).show();
-	  }
+	}
+
+	@Override
+	public void onDestroy() {
+		locationManager.removeUpdates(locationListener);
+		super.onDestroy();
+		// Toast.makeText(getApplicationContext(),
+		// "stopped sending location updates to ShareTaxiServer",
+		// Toast.LENGTH_SHORT).show();
+	}
 
 	@Override
 	public IBinder onBind(Intent arg0) {
-		//this service does not support binding. 
+		// this service does not support binding.
 		return null;
 	}
 
-	
-	
-	
-		  
-	
-	
 }
